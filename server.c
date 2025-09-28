421f69daea423135f484fbce7052f64e5c2e6d71
godine-png <godine_0@proton.me>
2025-09-27 21:15:43 -0600

    main and function

---
+++ server.c
@@ -0,0 +1,12 @@
+/* server side code */
+int do_server_work() {
+  int aux_fd;
+  return 0;
+}
+
+int main(int argc, char **argv) {
+  if (do_server_work() < 0) {
+    return 1;
+  }
+  return 0;
+}
