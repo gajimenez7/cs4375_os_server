eab453b83da77537e78ef18628fb91f216539db0
godi-png <eraticsyndicate@gmail.com>
2025-09-29 22:09:29 -0600

    figuring out no args

--- server.c
+++ server.c
@@ -13,6 +13,9 @@ int better_write(int fd, const void *buf, size_t size) {
   size_t bytes_to_write, bytes_already_written, bytes_written_this_time;
   ssize_t res_write;
 
+  printf("fd parameter: %d\n", fd);
+  printf("buf parameter: %s\n", (char *)buf);
+  printf("size parameter: %d\n", (int)size);
   bytes_to_write = size;
   bytes_already_written = (size_t)0;
   while (bytes_to_write > ((size_t)0)) {
@@ -30,8 +33,10 @@ int better_write(int fd, const void *buf, size_t size) {
 }
 
 int convert_port_name(uint16_t *port, const char *prog_name) {
-  if (better_write(1, prog_name, sizeof(prog_name)) < 0) {
-    fprintf(stderr, "Error printing program name or something\n");
+  if (better_write(STDOUT_FILENO, prog_name, sizeof(prog_name)) < 0) {
+    printf("prog_name: %s\n", (char *)prog_name);
+    printf("prog_name size: %d\n", (int)sizeof(prog_name));
+    fprintf(stderr, "Error converting port name.\n");
     return 1;
   }
   return 0;
@@ -42,7 +47,7 @@ int run_server_tool(const int aux_fd) { return 0; }
 int main(int argc, char **argv) {
   uint16_t port;
   int fd_param;
-  if (argc > 1) {
+  if (argc < 2) {
     usage();
     return 1;
   }
